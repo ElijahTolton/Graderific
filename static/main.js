16d21704f7a9f9b3aeec93fef93869ebf49de57c
ElijahTolton <156880246+ElijahTolton@users.noreply.github.com>
2025-04-06 20:47:35 -0600

    A6 Phase 4 and 5.
    Added Asyrnchous file upload. It allows error message to be displayed if file fails to upload.
    
    Added hypothesing grades. Add text box that allow the user to guess what grades they may get on an assignment.

--- static/main.js
+++ static/main.js
@@ -23,7 +23,10 @@ function make_table_sortable(table){
     });
 }
 
-make_table_sortable(document.querySelector("table.sortable"));
+const sortableTable = document.querySelector("table.sortable");
+if (sortableTable) {
+    make_table_sortable(sortableTable);
+}
 
 /**
  * Reorders table data when the last header cell is clicked on.
@@ -96,3 +99,167 @@ function parseData(tableDataText){
         return parseFloat(tableDataText);
     }
 }
+
+const form = document.querySelector("form.formSubmit");
+if(form){
+    make_form_async(form);
+}
+
+async function make_form_async(form){
+
+    // Add an eventListener
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const message = form.querySelector("span.message");
+        try{
+            message.innerText = "";
+            const formData = new FormData(form);
+            const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+            const respone = await fetch(form.action, {
+                method: "POST",
+                body: formData,
+                headers: {
+                    'X-CSRFToken': csrfToken
+                },
+
+            });
+            if(!respone.ok){
+                const text = await respone.text();
+                message.innerText = "Upload failed:" + text;
+            }else{
+                // If the file loaded correctly reload the page.
+                location.reload();
+            }
+            form.appendChild(message);
+
+        }catch( error ){
+            message.innerText = "Upload failed:" + error.message;
+            form.appendChild(message);
+        }
+    });
+}
+
+if (sortableTable && document.getElementById("testGrade")) {
+    make_grade_hypothesized(sortableTable);
+}
+
+function make_grade_hypothesized(table){
+    const button = document.getElementById("testGrade");
+
+    button.addEventListener("click", ()=>{
+        const isHypothesized = table.id == "Hypothesized"
+
+        // Switch toggle to other id value
+        if(isHypothesized)
+            table.id = "";
+        else
+            table.id = "Hypothesized";
+
+        button.textContent = isHypothesized ? "Hypothesize" : "Actual grades" ;
+
+        if(!isHypothesized){
+            convertToInputs(table);
+        } else{
+            restoreGrades(table);
+        }
+
+        computeGrades(table);
+    });
+}
+
+/**
+ * Change all Not Due or Ungraded values in the table to input elements
+ * @param {*} table 
+ */
+function convertToInputs(table){
+    const rows = table.querySelectorAll("tbody tr");
+
+    rows.forEach(row => {
+        const gradeCell = row.querySelector(".grade");
+
+        if (gradeCell && (gradeCell.textContent.trim() == "Not Due" || gradeCell.textContent.trim() == "Ungraded")){
+            const originalText = gradeCell.textContent;
+            const input = document.createElement("input");
+            input.type = "number";
+            input.min = 0;
+            input.max = 100;
+            input.placeholder = "";
+            input.classList.add("hypotheticalInput");
+            
+            // Keep track of the origanl values to restore
+            gradeCell.dataset.original = originalText;
+            gradeCell.textContent = "";
+            gradeCell.appendChild(input);
+
+            input.addEventListener("keyup", ()=> computeGrades(table));
+        }
+    });
+}
+
+/**
+ * Restore all grades to orignal Not Due or Ungraded.
+ * @param {*} table 
+ */
+function restoreGrades(table){
+    const input = table.querySelectorAll("input.hypotheticalInput");
+
+    input.forEach(input =>{
+        const cell = input.parentElement;
+        const original = cell.dataset.original;
+        cell.textContent = original;
+        delete cell.dataset.original;
+    });
+}
+
+/**
+ * Compute grades everytime a keystroke is done in input box.
+ * @param {*} table 
+ */
+function computeGrades(table){
+    var totalWeight = 0;
+    var totalPoints = 0;
+
+    const rows = table.querySelectorAll("tbody tr");
+    rows.forEach(row => {
+        const gradeCell = row.querySelector(".grade");
+        const weight = parseFloat(gradeCell.dataset.weight);
+        var score;
+
+        if(table.id == "Hypothesized"){
+            const input = gradeCell.querySelector("input");
+            if(input){
+                score = parseFloat(input.value);
+            }else if(gradeCell.textContent == "Missing"){
+                score = 0;
+            } else{
+                score = parseFloat(gradeCell.dataset.value);
+            }
+        }else{
+            const value = gradeCell.textContent.trim();
+            try{
+                if(value == "Missing"){
+                    score = 0;
+                }
+                else{
+                    score = parseFloat(value);
+                }
+            }catch (error){
+                score = 0;
+            }
+        }
+
+        if( !isNaN(score)){
+            totalPoints += (score * (weight / 100));
+            totalWeight += weight;
+        }
+    });
+
+    const finalGrade = (totalPoints / totalWeight) * 100
+    const finalGradeStr = Number(finalGrade.toFixed(1));
+    const finalGradeCell = document.getElementById("finalGrade");
+
+    if(finalGradeCell){
+        finalGradeCell.textContent = finalGradeStr + "%";
+    }
+}
+
